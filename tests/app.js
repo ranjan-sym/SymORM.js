87a5d821845a797e8b9f31cf282cebd595b47568
Ranjan Shrestha <ranjan.sym@gmail.com>
2015-08-26 02:39:30 +0545

    Started testing with react

--- tests/app.js
+++ tests/app.js
@@ -1,3 +1,24 @@
 /**
  * Created by ranjan on 8/26/15.
  */
+var Example = React.createClass({
+  getInitialState:function() {
+    return {
+      name: 'Not updated yet'
+    }
+  },
+
+  componentDidMount: function() {
+    SymORM.registerReactComponent(this, new SymORM.Event('station', 101, 'name'));
+  },
+
+  componentWillUnmount: function() {
+    SymORM.unregisterReactComponent(this);
+  },
+
+  render: function() {
+    return <div>{this.state.name}</div>;
+  }
+});
+
+React.render(<Example />, document.getElementById("container"));
